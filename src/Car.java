e0e164ccc3413a42e5c3a1bdefc14604bb72d753
Светка <sveta.chakrygina@mail.ru>
2022-10-19 21:31:34 +0300

    Третья задача

--- src/Car.java
+++ src/Car.java
@@ -7,13 +7,37 @@ public class Car {
     String productionCountry;
 
     public Car(String brand, String model, double engineVolume, String color, int productionYear, String productionCountry) {
-        this.brand = brand;
-        this.model = model;
-        this.engineVolume = engineVolume;
-        this.color = color;
-        this.productionYear = productionYear;
-        this.productionCountry = productionCountry;
-    }
+        if (brand == null) {
+            this.brand = "default";
+        } else {
+            this.brand = brand;
+        }
+            if (model == null) {
+                this.brand = "default";
+            } else {
+                this.model = model;
+            }
+                if (Double.compare(engineVolume,0) == 0) {
+                    this.engineVolume = 1.5;
+                } else {
+                    this.engineVolume = engineVolume;
+                }
+                    if (color == null) {
+                        this.color = "белый";
+                    } else {
+                        this.color = color;
+                    }
+                        if (productionYear == 0) {
+                            this.productionYear = 2000;
+                        } else {
+                            this.productionYear = productionYear;
+                        }
+                            if (productionCountry == null) {
+                                this.productionCountry = "default";
+                            } else {
+                                this.productionCountry = productionCountry;
+                            }
+                        }
 
     void car() {
         System.out.println("Модель и марка машины: " + brand + " " + model + ", " + " год выпуска: " + productionYear + ", " + " сборка: " + productionCountry + ", " + " цвет: " + color + ", " + " объем двигателя - " + engineVolume);
--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 public class Main {
     public static void main(String[] args) {
 
-        Car ladaGrande = new Car("Lada", "Grande",1.7, "желтый", 2015, "Россия");
+        Car ladaGrande = new Car("Lada", "Grande",0, "желтый", 2015, "Россия");
         Car audiA8 = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");;
-        Car bmwZ8 = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");;
-        Car kiaSportage4 = new Car("Kia", "Sportage 4 поколения", 2.4, "красный", 2018, "Южная Корея");;
+        Car bmwZ8 = new Car("BMW", "Z8", 3.0, "черный", 0, "Германия");;
+        Car kiaSportage4 = new Car(null, "Sportage 4 поколения", 2.4, "красный", 2018, "Южная Корея");;
         Car hyundaiAvante = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016, "Южная Корея");;
 
         ladaGrande.car();
